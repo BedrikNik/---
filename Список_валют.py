569bc0e7c114abacc27a4a7d829a7d9beb30805b
BedrikNik <122639623+BedrikNik@users.noreply.github.com>
2023-02-17 02:54:57 +0300

    Add files via upload

---
+++ Список_валют.py
@@ -0,0 +1,7 @@
+currency = {
+    'USD': ('USD', 'ДОЛЛАР', '$'),
+    'EUR': ('EUR', 'ЕВРО', '€'),
+    'RUB': ('RUB', 'РУБЛЬ', 'РУБ', 'Р'),
+}
+
+url_get = ('https://min-api.cryptocompare.com/data/price?fsym=', '&tsyms=')
