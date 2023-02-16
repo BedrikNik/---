f8d500a572f75dfd67475260626b66f9a2a2c06f
BedrikNik <122639623+BedrikNik@users.noreply.github.com>
2023-02-17 02:56:40 +0300

    Delete Список_валют.py

--- Список_валют.py
+++
@@ -1,7 +0,0 @@
-currency = {
-    'USD': ('USD', 'ДОЛЛАР', '$'),
-    'EUR': ('EUR', 'ЕВРО', '€'),
-    'RUB': ('RUB', 'РУБЛЬ', 'РУБ', 'Р'),
-}
-
-url_get = ('https://min-api.cryptocompare.com/data/price?fsym=', '&tsyms=')
